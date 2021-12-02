cb866a92d4c958fa3d400c1d31f2319b15c21008
izdihar1 <izdihar.khrifech@enis.tn>
2021-12-02 18:00:50 +0100

    ajout de room dans la partie admin

---
+++ E-Enchere Admin/Controllers/RoomController.cs
@@ -0,0 +1,112 @@
+﻿using E_Enchere_Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Enchere_Admin.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly EEnchereContext _db;
+        public RoomController(EEnchereContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Room> objList = _db.Rooms;
+
+
+            return View(objList);
+        }
+        // get create
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> TypeDropDown = _db.Articles.Select(i => new SelectListItem
+            {
+                Text = i.Nom,
+                Value = i.IdArticle.ToString(),
+            });
+
+            ViewBag.TypeDropDown = TypeDropDown;
+            return View();
+        }
+        // post create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Room obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Rooms.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+        }
+        //get delete
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Rooms.Find(Id);
+            if (obj == null) { return NotFound(); }
+            return View(obj);
+
+
+
+        }
+        //post delete
+        [HttpPost]
+        public IActionResult DeleteRoom(int? Id)
+        {
+            var obj = _db.Rooms.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Rooms.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        //get update
+        public IActionResult Update(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Rooms.Find(Id);
+            if (obj == null) { return NotFound(); }
+            return View(obj);
+
+
+
+        }
+        // post update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Room obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Rooms.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+        }
+    }
+}
