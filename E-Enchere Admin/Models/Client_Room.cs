f5747cac16a8776b45d961aeda9ff32057b969d3
izdihar1 <izdihar.khrifech@enis.tn>
2021-12-02 19:02:33 +0100

    amelioration

---
+++ E-Enchere Admin/Models/Client_Room.cs
@@ -0,0 +1,17 @@
+﻿using E_Enchere_Admin.Models;
+using eEnchere.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eEnchere.Data
+{
+    public class Client_Room
+    {
+        public int IdClient { get; set; }
+        public Client Client { get; set; }
+        public int IdRoom { get; set; } 
+        public Room  Room { get; set; }
+    }
+}
